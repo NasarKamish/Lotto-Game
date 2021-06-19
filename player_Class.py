3696d4c41640f08382132b162fc4085308878821
NasarKamish <Nasarkamish@gmail.com>
2021-06-19 14:23:14 +0200

    U

--- player_Class.py
+++ player_Class.py
@@ -14,14 +14,17 @@ class Player:
         self.dict = {"Name": name, "Surname": surname, "Age": age, "ID": id, "Email": email, "Currency": currency, "Prize": prize, "Player ID": player_ID}
 
     def text_fill(self):
-        with open("player.txt", "w") as player_file:
+        with open(str(self.dict["Player ID"]) + ".txt", "w") as player_file:
             player_file.write(json.dumps(self.dict))
 
     def text_get(self):
-        with open("player.txt", "r") as player_file:
+        with open(self.dict["Player ID"] + ".txt", "r") as player_file:
             player_details = json.loads(player_file.read())
             return player_details
 
+    def player_id_store(self):
+        with open("player.txt", "w") as player_id:
+            player_id.write(json.dumps(self.player_ID))
 
 # player = Player("Nasar", "Kamish", 18, "012839", "nasarkamish@gmail.com", "ZAR", 0, "Nk2014")
 # player.text_fill()
